c2acb6adeaef75daeb66208975b89bd0b3ec96a4
AlanTubio <alannahuelmiya2@hotmail.com>
2023-04-16 23:38:03 -0300

    corse error

--- src/main/java/com/SpringBoot/Portofolio/controller/PersonaController.java
+++ src/main/java/com/SpringBoot/Portofolio/controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-df72c.web.app")
 public class PersonaController {
     @Autowired
     private IPersonaService interPersona;
